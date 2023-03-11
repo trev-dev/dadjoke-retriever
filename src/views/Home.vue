3a3f69ab1dd69649ad1eba18f90e1baed9d70456
Trevor Richards <trev@trevdev.ca>
2023-03-10 21:41:04 -0800

    add typed doc to getRandomArrayValue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -93,6 +93,13 @@ const icons = [
   'mdi-thumb-down'
 ];
 
+/**
+ * Get a random value from an array.
+ *
+ * @template T
+ * @param {Array<T>} arr
+ * @returns {T} a random array index value
+ */
 function getRandomArrayValue(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
